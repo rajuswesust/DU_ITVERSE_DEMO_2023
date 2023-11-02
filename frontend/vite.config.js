d75e6a3424e556054de535a17458be5810352d8b
Mahmud <mahmudur69@student.sust.edu>
2023-11-02 16:25:17 +0600

    initial setup

---
+++ frontend/vite.config.js
@@ -0,0 +1,33 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+import { defineConfig, loadEnv } from "vite";
+
+import http from "https";
+import react from '@vitejs/plugin-react'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_HOST,
+          changeOrigin: true,
+          secure: false,
+          agent: new http.Agent(),
+        },
+      },
+    },
+  };
+});
