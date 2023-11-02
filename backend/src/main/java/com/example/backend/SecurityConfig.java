83c4cc00423db6fe96e65c580eba39af93f37bad
rajuswesust <singhoraju8@gmail.com>
2023-11-02 17:12:42 +0600

    test change

--- backend/src/main/java/com/example/backend/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)
             throws Exception {
+
         http.authorizeHttpRequests((authz) -> authz
                 .requestMatchers(new AntPathRequestMatcher("/**"))
                 .permitAll());
